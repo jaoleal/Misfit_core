7c9b62e447bc2ef30c06456b65ce80d255713a10
j-moreno-c-r <joaomcr@proton.me>
2025-02-20 17:20:52 -0300

    trying to made the block

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// src/main.rs
+
+mod block;
+
 fn main() {
-        println!("Hello, world!");
+    let block_header = block::BlockHeader::new();
+
+    println!("Constructed Block Header:");
+    println!("Version: {}", block_header.version);
+    println!("Previous Block Hash: {:?}", block_header.prev_block_hash);
+    println!("Merkle Root: {:?}", block_header.merkle_root);
+    println!("Timestamp: {}", block_header.timestamp);
+    println!("Difficulty Target: {}", block_header.difficulty_target);
+    println!("Nonce: {}", block_header.nonce);
 }
