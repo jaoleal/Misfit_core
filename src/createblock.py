0adc05c6cf5fb6d3fa77f5040d091957f9daf679
TheMhv <mhv.geral@gmail.com>
2025-02-21 13:06:53 -0300

    Feat: Initial setup for commands files

--- src/createblock.py
+++ src/createblock.py
@@ -1,19 +1,23 @@
 # TODO:
 #   - Import a lib (for save time)
-#   - Create a valid raw transaction
-#   - Split the transaction
-#   - Replace transaction parameters by misfit parameters 
-#   - Assemble misfit transaction 
+#   - Create a valid raw block
+#   - Split the block
+#   - Replace block parameters by misfit parameters 
+#   - Assemble misfit block
 
-class CreateTx:
+class CreateBlock:
+    self.version: bool = False
+    self.prevblock: bool = False
+    self.merkleroot: bool = False
+    self.timestamp: bool = False
+    self.bits: bool = False
+    self.nonce: bool = False
     self.tx_count: int = 0
     self.invalid_tx_count: int = 0
     self.tx_version: bool = False
     self.tx_marker: bool = False
     self.tx_flag: bool = False
     self.tx_locktime: bool = False
-    
-    # Inputs
     self.tx_in_count: int = 0
     self.invalid_tx_in_count: int = 0
     self.tx_in_txid: bool = False
@@ -21,37 +25,32 @@ class CreateTx:
     self.tx_in_script_size: int = 0
     self.tx_in_script: bool = False
     self.tx_in_sequence: bool = False
-    
-    # Outputs
     self.tx_out_count: int = 0
     self.invalid_tx_out_count: int = 0
     self.tx_out_amount: int = 0
     self.tx_out_script_size: int = 0
     self.tx_out_script: bool = False
-
-    # Witness
     self.tx_witness_count: int = 0
     self.invalid_tx_witness_count: int = 0
     self.tx_witness_size: int = 0
     self.tx_witness_item: bool = False
-
+    
     def __init__(self, **args):
         # TODO: Set class variables by arguments
         return
 
-    def create(self):
-        # TODO: Create a valid raw transaction
-        return 
-
-    def split(self):
-        # TODO: Split raw transaction
+    def create():
+        # TODO: Create a valid raw block
         return
 
-    def replace(self):
+    def split():
+        # TODO: Split the block
+        return
+
+    def replace():
         # TODO: Replace parameters by misfit params
         return
-    
-    def assemble(self):
-        # TODO: Assemble misfit transaction
-        return
 
+    def assemble():
+        # TODO: Assemble misfit block
+        return
