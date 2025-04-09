e0eb72611975755a5d71594d0f61fd4f3da4c449
TheMhv <mhv.geral@gmail.com>
2025-04-09 16:40:19 -0300

    Fix: Remove terget folder

--- target/debug/build/proc-macro2-d1fb3181e530b33b/build_script_build-d1fb3181e530b33b.d
+++
@@ -1,5 +0,0 @@
-/home/joaozinho/Misfit_core/target/debug/build/proc-macro2-d1fb3181e530b33b/build_script_build-d1fb3181e530b33b: /home/joaozinho/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.94/build.rs
-
-/home/joaozinho/Misfit_core/target/debug/build/proc-macro2-d1fb3181e530b33b/build_script_build-d1fb3181e530b33b.d: /home/joaozinho/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.94/build.rs
-
-/home/joaozinho/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.94/build.rs:
--- target/debug/deps/is_terminal_polyfill-1ddc9b0610a04800.d
+++
@@ -1,7 +0,0 @@
-/home/joaozinho/Misfit_core/target/debug/deps/libis_terminal_polyfill-1ddc9b0610a04800.rmeta: /home/joaozinho/.cargo/registry/src/index.crates.io-6f17d22bba15001f/is_terminal_polyfill-1.70.1/src/lib.rs
-
-/home/joaozinho/Misfit_core/target/debug/deps/libis_terminal_polyfill-1ddc9b0610a04800.rlib: /home/joaozinho/.cargo/registry/src/index.crates.io-6f17d22bba15001f/is_terminal_polyfill-1.70.1/src/lib.rs
-
-/home/joaozinho/Misfit_core/target/debug/deps/is_terminal_polyfill-1ddc9b0610a04800.d: /home/joaozinho/.cargo/registry/src/index.crates.io-6f17d22bba15001f/is_terminal_polyfill-1.70.1/src/lib.rs
-
-/home/joaozinho/.cargo/registry/src/index.crates.io-6f17d22bba15001f/is_terminal_polyfill-1.70.1/src/lib.rs:
