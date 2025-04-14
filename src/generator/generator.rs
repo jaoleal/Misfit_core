9efc9a9bfdce989d9a9137933dd8fb5736ecb91b
j-moreno-c-r <joaomcr@proton.me>
2025-04-14 15:39:15 -0300

    api fix

--- src/generator/generator.rs
+++ src/generator/generator.rs
@@ -1,35 +1,55 @@
-use super::generate_tx::GenerateTx;
+
 use super::generate_blocks::GenerateBlock;
-pub struct Generator {
-}
+use super::generate_tx::GenerateTx;
+pub struct Generator {}
 
 impl Generator {
-    pub fn generate_from_input(input:i32) -> String{
-        if input == 1{
-            let (raw_tx, txid) = GenerateTx::generate_simple_p2wpkh();
-            let final_structured: String = [format!("Raw Transaction 🥩: {}", raw_tx).to_string() , format!("TXID 🪪 : {}", txid).to_string()].join("\n---\n");
-            final_structured
-        }else if input > 1{//if the user request more than one transaction we return a block
-            let mut raw_tx:Vec<String> = vec![];
-            let mut txid:Vec<String> = vec![];
-            for _c in 0..input{
-                let (new_raw_tx, new_txid) = GenerateTx::generate_simple_p2wpkh();
-                raw_tx.push(new_raw_tx);
-                txid.push(new_txid);
+    pub fn generate(input: i32) -> String {
+        match input {
+            1 => {
+                let (raw_tx, txid) = GenerateTx::generate_simple_p2wpkh();
+                [
+                    format!("Raw Transaction 🥩: {}", raw_tx).to_string(),
+                    format!("TXID 🪪 : {}", txid).to_string(),
+                ]
+                .join("\n---\n")
             }
-            let block_header =GenerateBlock::new(txid.clone());
-            let final_structured = [format!("Blockheader Info 🧊: {:#?} ", block_header),format!("Raw transactions used in it:{:#?}", raw_tx),format!("Used Txids: {:#?}", txid)].join("\n---\n");
-            final_structured
-            }else{
-            return "Your input is invalid, try again with a valid number of transactions 😕".to_string()
+
+            n if n > 1 => {
+                //if the user request more than one transaction we return a block
+                let mut raw_tx: Vec<String> = vec![];
+                let mut txid: Vec<String> = vec![];
+
+                for _c in 0..input {
+                    let (new_raw_tx, new_txid) = GenerateTx::generate_simple_p2wpkh();
+                    raw_tx.push(new_raw_tx);
+                    txid.push(new_txid);
+                }
+
+                let block_header = GenerateBlock::new(txid.clone());
+
+                [
+                    format!("Blockheader Info 🧊: {:#?} ", block_header),
+                    format!("Raw transactions used in it:{:#?}", raw_tx),
+                    format!("Used Txids: {:#?}", txid),
+                ]
+                .join("\n---\n")
+            }
+
+            _ => "Your input is invalid, try again with a valid number of transactions 😕"
+                .to_string(),
         }
     }
-    pub fn proces_flags_to_broke(flags:Vec<String>) ->String{
+
+    pub fn proces_flags_to_broke(flags: Vec<String>) -> String {
         let mut flags_concateneted = "".to_string();
-        for c in flags{
+
+        for c in flags {
             flags_concateneted += &c;
         }
-        return format!("When cant process you flags for now {}",flags_concateneted ).to_string()
-    }
-}
 
+        format!("When cant process you flags for now {}", flags_concateneted).to_string()
+    }
+
+
+}
