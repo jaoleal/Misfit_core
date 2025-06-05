ed35330cabb449a1c6900bb9c163f0c722538b20
j-moreno-c-r <joaomcr@proton.me>
2025-06-05 15:56:45 -0300

    medina changes in my api

--- src/api.rs
+++ src/api.rs
@@ -3,32 +3,38 @@ use bitcoin::block::Header;
 use bitcoin::Transaction;
 use std::collections::HashSet;
 use misfit_core::transaction::random::transaction::TxParams;
+use misfit_core::block::random::block::BlockParams;
 use misfit_core::transaction::generator::GenerateTx;
-use misfit_core::block::generate_blocks::GenerateBlock;
+use misfit_core::block::generator::GenerateBlock;
 use misfit_core::breakers::{decoder_tools, transaction, block};
 
 pub struct Generator {}
 
 impl Generator {
     pub fn block(tx_count: u32) -> String {
+        let mut txs: Vec<Transaction> = vec![];
         let mut raw_tx: Vec<String> = vec![];
-        let mut txid: Vec<String> = vec![];
+        let mut tx_ids: Vec<String> = vec![];
 
         for _c in 0..tx_count {
             let tx = GenerateTx::valid_random(TxParams::default());
             let raw_transaction = hex::encode(encode::serialize(&tx)).to_string();
-            let tx_id = tx.compute_txid().to_string();
+            let txid = tx.compute_txid().to_string();
 
+            txs.push(tx);
             raw_tx.push(raw_transaction);
-            txid.push(tx_id);
+            tx_ids.push(txid);
         }
 
-        let block_header = GenerateBlock::new(txid.clone());
+        let block = GenerateBlock::valid_random(BlockParams {
+            header: None,
+            txs: Some(txs),
+        });
 
         [
-            format!("Blockheader Info 🧊: {:#?} ", block_header),
-            format!("Raw transactions used in it:{:#?}", raw_tx),
-            format!("Used Txids: {:#?}", txid),
+            format!("{:#?} ", block.header),
+            format!("Raw txs: {:#?}", raw_tx),
+            format!("TxID: {:#?}", tx_ids),
         ]
         .join("\n---\n")
     }
